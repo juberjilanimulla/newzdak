f00f770d8e31927aaed9f6d9c4b3829a9fc9b95a
Juber_Mulla <juberjilanimulla@gmail.com>
2025-08-07 13:22:20 +0530

    this is first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import dbConnect from "./db.js";
+import config from "./config.js";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = config.PORT;
+
+app.set("trust proxy", true);
+morgan.token("remote-addr", function (req) {
+  return req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+});
+
+morgan.token("url", (req) => {
+  const url = new URL(req.url, `http://${req.headers.host}`);
+  return req.originalUrl;
+});
+
+app.use(
+  morgan(
+    ":remote-addr :method :url :status :res[content-length] - :response-time ms"
+  )
+);
+
+app.use(express());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: "10mb" }));
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ extended: true, limit: "100mb" }));
+
+app.use((err, req, res, next) => {
+  if (err instanceof SyntaxError && err.status === 400 && "body" in err) {
+    return res.status(400).json({ error: "Invalid JSON input" });
+  }
+  next(err); // Pass to the next middleware if not a JSON error
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ error: "Internal server error" });
+});
+
+//routes
+
+//database connected successfullys
+dbConnect()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`server listening at ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log("unable to connected to server", error);
+  });
